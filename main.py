c5850919ea35fff25e6d3ad6f6df568a0c9fc06e
KuriosD <doriatchi@gmail.com>
2026-05-15 13:48:13 +0300

    Added TODO and demo code to main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 # this is where we will use both of our .py files
 # to train the model
+
+# TO-DO
+# TRAIN / TEST
+# CROSS-VALIDATION
+# ENSEMBLE
+
+from sklearn.model_selection import train_test_split
+import preprocessing as pp
+
+stop_words = pp.get_nltk_data()
+x, y = pp.csv_to_dataframe(stop_words)
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
